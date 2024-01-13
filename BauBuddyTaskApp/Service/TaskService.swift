aa95ed8fad422fa9069d24508f023c34b9fa8f18
ezgiik <ezgiikarabay@hotmail.com>
2024-01-13 03:52:09 +0300

    fix redirect

--- BauBuddyTaskApp/Service/TaskService.swift
+++ BauBuddyTaskApp/Service/TaskService.swift
@@ -14,7 +14,8 @@ class TaskService {
         request.httpMethod = "GET"
         request.addValue("Bearer \(auth.accessToken)", forHTTPHeaderField: "Authorization")
 
-        let session = URLSession.shared
+
+       let session = URLSession.shared
         let dataTask = session.dataTask(with: request) { (data, response, error) in
             if let error = error {
                 completion(.failure(error))
@@ -26,8 +27,21 @@ class TaskService {
                     completion(.failure(error))
                 }
             }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 401 {
+                    DispatchQueue.main.async {
+                        // Here you would need to implement the logic to redirect to the LoginView.
+                        // For example, you can use NotificationCenter to post a notification
+                        // that the SceneDelegate or AppDelegate listens for to change the rootViewController,
+                        // or if you have a reference to the navigationController you can pop to the desired viewController.
+                        NotificationCenter.default.post(name: NSNotification.Name("UnauthorizedAccessDetected"), object: nil)
+                    }
+                }
+            }
         }
 
-        dataTask.resume()
+
+       dataTask.resume()
     }
 }
